148a646b02ce40451b6024a193024d7d87229acc
Filip Kotlas <filip.kotlas.fk@gmail.com>
2025-02-28 11:29:55 +0100

    Tweak plotting of datat.

--- ode-solve.h
+++ ode-solve.h
@@ -25,7 +25,6 @@ bool solve( const double initialTime,
    const int timeStepsCount = std::ceil( std::max( 0.0, finalTime - initialTime ) / timeStep );
    double time( initialTime );
    problem->writeSolution( time, 0, u );
-   auto start = std::chrono::high_resolution_clock::now();
 
    for( int k = 1; k <= timeStepsCount; k++ )
    {
@@ -43,16 +42,16 @@ bool solve( const double initialTime,
       auto stepEnd = std::chrono::high_resolution_clock::now();
       std::chrono::duration<double> stepDuration = stepEnd - stepStart;
       
-      double average_time_per_step = (std::chrono::duration<double>(stepEnd - start).count() / k);
-      double remaining_time = average_time_per_step * (timeStepsCount - k);
+      double time_per_step = (std::chrono::duration<double>(stepEnd - stepStart).count());
+      double remaining_time = time_per_step * (timeStepsCount - k);
       
       int hours = static_cast<int>(remaining_time) / 3600;
       int minutes = (static_cast<int>(remaining_time) % 3600) / 60;
       int seconds = remaining_time - (hours * 3600 + minutes * 60);
       
-      std::cout << "Step " << k << " / " << timeStepsCount << " => " << std::fixed
+      std::cout << "Steps completed: " << k << " / " << timeStepsCount << " => " << std::fixed
                 << std::setprecision(2) << ( double ) k / ( double ) timeStepsCount * 100.0 << "% ";
-      std::cout << "     time remaining: " 
+      std::cout << "     Time remaining: " 
                 << hours << "h " << minutes << "m " << seconds << "s"
                 << std::endl;
    }
--- plot.py
+++ plot.py
@@ -1,13 +1,29 @@
 from plotter import ScatterPlotter2D, SurfacePlotter
 
-input_folder = "Results_rough_mesh_a_10"
-dimension_2 = False
+input_folders = ["Results_rough_mesh_a_10", "Results_finer_mesh_a_10"]
+dimension_2 = True
 
+"""
 plotter = None
-dimension_3 = not dimension_2
 if dimension_2:
     plotter = ScatterPlotter2D(input_folder, False, True, True)
-if dimension_3:
+else:
     plotter = SurfacePlotter(input_folder, True, False, False)
+"""
 
-plotter.save_animation()
+plotter = None
+for folder in input_folders:
+
+    plotter = ScatterPlotter2D(folder, False, True, True)
+    plotter.save_frame(0)
+    plotter.save_frame(10)
+    plotter.save_frame(100)
+    plotter.save_frame(300)
+    plotter.save_animation()
+
+    plotter = SurfacePlotter(folder, True, False, False)
+    plotter.save_frame(0)
+    plotter.save_frame(10)
+    plotter.save_frame(100)
+    plotter.save_frame(300)
+    plotter.save_animation()
