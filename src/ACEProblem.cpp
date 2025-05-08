df609992566541a0e2d18090f3d67e7dc909320e
Filip Kotlas <filip.kotlas.fk@gmail.com>
2025-05-08 18:14:49 +0200

    Switch finite differences in div_D_grad functions to central with half nodes.

--- src/ACEProblem.cpp
+++ src/ACEProblem.cpp
@@ -330,40 +330,32 @@ double ACEProblem::grad_norm(double *u, int i, int j)
 
 double ACEProblem::div_D_grad_concentration(double *u, int i, int j)
 {
-	double x_direction = 0;
-	double y_direction = 0;
-	if(i > 1 && i < sizeX-2)
-	{
-		x_direction = get_conc_diff_coef(u, i+1, j) * (conc_at(u, i+2, j) - conc_at(u, i, j)) / 2 / hx
-						 - get_conc_diff_coef(u, i-1, j) * (conc_at(u, i, j) - conc_at(u, i-2, j)) / 2 / hx;
-		x_direction /= 2;
-	}
-	else
-	{
-		x_direction = get_conc_diff_coef(u, i + 1, j) * (conc_at(u, i+1, j) - conc_at(u, i, j)) / hx
-					  - get_conc_diff_coef(u, i, j) * (conc_at(u, i, j) - conc_at(u, i-1, j)) / hx;
-	}
-	if(j > 1 && j < sizeY-2)
-	{
-		y_direction = get_conc_diff_coef(u, i, j+1) * (conc_at(u, i, j+2) - conc_at(u, i, j)) / 2 / hy
-					  - get_conc_diff_coef(u, i, j-1) * (conc_at(u, i, j) - conc_at(u, i, j-2)) / 2 / hy;
-		y_direction /= 2;
-	}
-	else
-	{
-		y_direction = get_conc_diff_coef(u, i, j + 1) * (conc_at(u, i, j+1) - conc_at(u, i, j)) / hy
-                      - get_conc_diff_coef(u, i, j) * (conc_at(u, i, j) - conc_at(u, i, j-1)) / hy;
-	}
+	double coeff_plus_half = (get_conc_diff_coef(u, i + 1, j) + get_conc_diff_coef(u, i, j)) / 2;
+	double coeff_minus_half = (get_conc_diff_coef(u, i, j) + get_conc_diff_coef(u, i - 1, j)) / 2;
+	double x_direction = coeff_plus_half * (conc_at(u, i+1, j) - conc_at(u, i, j)) / hx
+				         - coeff_minus_half * (conc_at(u, i, j) - conc_at(u, i-1, j)) / hx;
+
+	coeff_plus_half = (get_conc_diff_coef(u, i, j+1) + get_conc_diff_coef(u, i, j)) / 2;
+	coeff_minus_half = (get_conc_diff_coef(u, i, j) + get_conc_diff_coef(u, i, j-1)) / 2;
+	double y_direction = coeff_plus_half * (conc_at(u, i, j+1) - conc_at(u, i, j)) / hy
+                      	 - coeff_minus_half * (conc_at(u, i, j) - conc_at(u, i, j-1)) / hy;
+
 	return x_direction / hx + y_direction / hy;
 }
 
 double ACEProblem::div_D_grad_phase(double *u, int i, int j)
 {
-   double x_direction = get_phas_diff_coef(u, i + 1, j) * (phase_at(u, i + 1, j) - phase_at(u, i, j)) / hx
-                        - get_phas_diff_coef(u, i, j) * (phase_at(u, i, j) - phase_at(u, i - 1, j)) / hx;
-   double y_direction = get_phas_diff_coef(u, i, j + 1) * (phase_at(u, i, j+1) - phase_at(u, i, j)) / hy
-                        - get_phas_diff_coef(u, i, j) * (phase_at(u, i, j) - phase_at(u, i, j-1)) / hy;
-   return x_direction / hx + y_direction / hy;
+	double coeff_plus_half = (get_conc_diff_coef(u, i + 1, j) + get_conc_diff_coef(u, i, j)) / 2;
+	double coeff_minus_half = (get_conc_diff_coef(u, i, j) + get_conc_diff_coef(u, i - 1, j)) / 2;
+	double x_direction = coeff_plus_half * (phase_at(u, i+1, j) - phase_at(u, i, j)) / hx
+				         - coeff_minus_half * (phase_at(u, i, j) - phase_at(u, i-1, j)) / hx;
+
+	coeff_plus_half = (get_conc_diff_coef(u, i, j+1) + get_conc_diff_coef(u, i, j)) / 2;
+	coeff_minus_half = (get_conc_diff_coef(u, i, j) + get_conc_diff_coef(u, i, j-1)) / 2;
+	double y_direction = coeff_plus_half * (phase_at(u, i, j+1) - phase_at(u, i, j)) / hy
+                      	 - coeff_minus_half * (phase_at(u, i, j) - phase_at(u, i, j-1)) / hy;
+
+	return x_direction / hx + y_direction / hy;
 }
 
 double ACEProblem::get_conc_diff_coef(double *u, int i, int j)
