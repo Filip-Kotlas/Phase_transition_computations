5f4ddcfa7e88246993a11d5cc6246e490562d0a0
Filip Kotlas <filip.kotlas.fk@gmail.com>
2025-07-16 19:23:20 +0200

    Insignificant changes.

--- python/basic_plot.py
+++ python/basic_plot.py
@@ -19,7 +19,7 @@ args = parser.parse_args()
 results_path = Path()
 results_path = results_path.parent / "results" / args.name
 
-boundary_plotter = BoundaryPlotter2D(results_path, False, True, False, "phase")
+boundary_plotter = BoundaryPlotter2D(results_path, False, True, True, "phase")
 phase_surface_plotter = SurfacePlotter(results_path, "phase")
 concentration_surface_plotter = SurfacePlotter(results_path, "concentration")
 concentration_cut_plotter = CutPlotter(results_path, "concentration", "x", 0.5, False)
@@ -60,4 +60,4 @@ if args.type in ("phase", "both"):
     boundary_plotter.save_animation()
 
 if args.type not in ("phase", "conc", "both"):
-    raise Exception("Wrong argument for type.")
+    raise ValueError("Wrong argument for type.")
