777e9c663de3e1922fb4a57d2187f3ada224f83e
Filip Kotlas <filip.kotlas.fk@gmail.com>
2025-08-01 14:40:27 +0200

    Improve basic_plotter

--- python/basic_plot.py
+++ python/basic_plot.py
@@ -19,37 +19,18 @@ args = parser.parse_args()
 results_path = Path()
 results_path = results_path.parent / "results" / args.name
 
-boundary_plotter = BoundaryPlotter2D(results_path, False, True, False, "phase")
+boundary_plotter = BoundaryPlotter2D(results_path, False, True, True, "phase")
 phase_surface_plotter = SurfacePlotter(results_path, "phase")
 concentration_surface_plotter = SurfacePlotter(results_path, "concentration")
 concentration_cut_plotter = CutPlotter(results_path, "concentration", "x", 0.5, False)
+for i in [0, 10, 50, 100, 500, 1000, 1600]:
+    if args.type in ("phase", "both"):
+        boundary_plotter.save_frame(i)
+        phase_surface_plotter.save_frame(i)
 
-if args.type in ("phase", "both"):
-    boundary_plotter.save_frame(0)
-    boundary_plotter.save_frame(10)
-    boundary_plotter.save_frame(20)
-    boundary_plotter.save_frame(50)
-    boundary_plotter.save_frame(100)
-
-    phase_surface_plotter.save_frame(0)
-    phase_surface_plotter.save_frame(10)
-    phase_surface_plotter.save_frame(20)
-    phase_surface_plotter.save_frame(50)
-    phase_surface_plotter.save_frame(100)
-
-if args.type in ("conc", "both"):
-    concentration_surface_plotter.save_frame(0)
-    concentration_surface_plotter.save_frame(10)
-    concentration_surface_plotter.save_frame(20)
-    concentration_surface_plotter.save_frame(50)
-    concentration_surface_plotter.save_frame(100)
-
-    concentration_cut_plotter.save_frame(0)
-    concentration_cut_plotter.save_frame(10)
-    concentration_cut_plotter.save_frame(20)
-    concentration_cut_plotter.save_frame(50)
-    concentration_cut_plotter.save_frame(100)
-
+    if args.type in ("conc", "both"):
+        concentration_surface_plotter.save_frame(i)
+        concentration_cut_plotter.save_frame(i)
 
 if args.type in ("conc", "both"):
     concentration_cut_plotter.save_animation()
