f4f833cd61d001ead7e5d6f9daf0d5d4c8ebb049
Filip Kotlas <kotlafi2@cvut.cz>
2025-10-07 16:59:06 +0200

    All in shambles, needs to be fixed, lot of work ahead.

---
+++ include/Problem.hpp
@@ -0,0 +1,20 @@
+#include "constants.hpp"
+#include "Parameters.hpp"
+
+template< typename Vector, typename Real = typename Vector::RealType, typename Index = typename Vector::IndexType >
+class Problem
+{
+public:
+    ACEProblem(int sizeX,
+               int sizeY,
+               Domain domain,
+               double alpha,
+               double beta,
+               double par_a,
+               double ksi,
+               MODEL model) {
+                    
+    }
+
+
+}
